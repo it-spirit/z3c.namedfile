c80aa2d212b87232bb040813abb9a4a5b61309c4
Thomas Massmann <thomas.massmann@inqbus.de>
2010-12-13 08:59:25 +0100

    Initial import.
    Created package structure.
    Added dependencies for blob support.

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+import os
+
+version = '0.1'
+
+setup(
+    name='z3c.namedfile',
+    version=version,
+    description="File types and fields for images, files and blob files " \
+                "with filenames.",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='zope named file image blob',
+    author='Thomas Massmann',
+    author_email='thomas.massmann@inqbus.de',
+    url='http://pypi.python.org/pypi/z3c.namedfile',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require = {
+        'blobs': ['z3c.blobfile', ],
+#         'scales': ['plone.scale[storage] >=1.1dev', ],
+    },
+    entry_points={
+    },
+)
