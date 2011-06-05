132a295889fc24ea747bce4d76ae03f21d3f968c
Thomas Massmann <thomas.massmann@inqbus.de>
2011-06-05 17:33:27 +0200

    Set version to 0.1.3. Updated documentation.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '0.1.2'
+version = '0.1.3'
 
 #---[ START Server locking]--------------------------------------------------
 LOCK_PYPI_SERVER = "http://mypypi.inqbus.de"
