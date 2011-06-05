27d575743e82d81060381a92efdf0f89bc298f5e
Thomas Massmann <thomas.massmann@inqbus.de>
2011-06-05 16:31:18 +0200

    Locked pypi to Inqbus private MyPyPI.

--- setup.py
+++ setup.py
@@ -1,9 +1,63 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
 import os
+import sys
+from setuptools import setup, find_packages
 
 version = '0.1.2'
+
+#---[ START Server locking]--------------------------------------------------
+LOCK_PYPI_SERVER = "http://mypypi.inqbus.de"
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+def check_server(server):
+    if not server:
+        return
+
+    COMMANDS_WATCHED = ('register', 'upload')
+
+    changed = False
+
+    for command in COMMANDS_WATCHED:
+        if command in sys.argv:
+            # Found one command, check for -r or --repository.
+            commandpos = sys.argv.index(command)
+            i = commandpos + 1
+            repo = None
+            while i < len(sys.argv) and sys.argv[i].startswith('-'):
+                # Check all following options (not commands).
+                if (sys.argv[i] == '-r') or (sys.argv[i] == '--repository'):
+                    # Next one is the repository itself.
+                    try:
+                        repo = sys.argv[i + 1]
+                        if repo.lower() != server.lower():
+                            print "You tried to %s to %s, while this package "\
+                                  "is locked to %s" % (command, repo, server)
+                            sys.exit(1)
+                        else:
+                            # Repo is OK.
+                            pass
+                    except IndexError:
+                        # End of args.
+                        pass
+                i = i + 1
+
+            if repo is None:
+                # No repo found for the command.
+                print "Adding repository %s to the command %s" % (
+                    server, command)
+                sys.argv[commandpos + 1: commandpos + 1] = ['-r', server]
+                changed = True
+
+    if changed:
+        print "Final command: %s" % (' '.join(sys.argv))
+
+check_server(LOCK_PYPI_SERVER)
+#---[ END Server locking]----------------------------------------------------
 
 setup(
     name='z3c.namedfile',
@@ -40,8 +94,5 @@ setup(
     ],
     extras_require = {
         'blobs': ['z3c.blobfile', ],
-#         'scales': ['plone.scale[storage] >=1.1dev', ],
-    },
-    entry_points={
     },
 )
