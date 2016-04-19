61fd4d321dbf9bfc1b161a2aa4503648fb51348d
Thomas Massmann <thomas.massmann@it-spir.it>
2016-04-19 14:47:38 +0200

    Clean up setup.py.

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 """Setup for z3c.namedfile package."""
-from setuptools import setup, find_packages
-import os
+
+from setuptools import (
+    find_packages,
+    setup,
+)
 
 version = '0.3.dev0'
+description = 'File types and fields for images, files and blob files ' \
+              'with filenames.'
 
+long_description = ('\n'.join([
+    open('README.rst').read(),
+    'Contributors',
+    '------------\n',
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+]))
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-long_description = (
-    read('README.rst')
-    + '\n' +
-    read('CHANGES.rst')
-)
+install_requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
+    'zope.app.file',
+]
 
 setup(
     name='z3c.namedfile',
     version=version,
-    description='File types and fields for images, files and blob files '
-                'with filenames.',
+    description=description,
     long_description=long_description,
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -29,32 +37,32 @@ setup(
         'Framework :: Zope3',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Zope",
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='zope named file image blob',
     author='Thomas Massmann',
     author_email='thomas.massmann@it-spir.it',
-    url='http://pypi.python.org/pypi/z3c.namedfile',
-    license='GPL',
+    url='https://bitbucket.org/it_spirit/z3c.namedfile',
+    download_url='http://pypi.python.org/pypi/z3c.namedfile',
+    license='BSD',
     packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'': 'src'},
     namespace_packages=['z3c'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'setuptools',
-        # -*- Extra requirements: -*-
-        # 'plone.scale',
-        'zope.app.file',
-    ],
-    extras_require={
-        'blobs': [
+    install_requires=install_requires,
+    extras_require=dict(
+        blobs=[
             'z3c.blobfile',
         ],
-        'scales': [
-            'plone.scale',
+        test=[
+            'Pillow',
         ],
-        'test': [],
-    },
+    ),
 )
