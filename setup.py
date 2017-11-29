b8ded096023c73d57402f64c5f2bb1bd8a72c18a
Thomas Massmann <thomas.massmann@it-spir.it>
2017-11-29 18:04:34 -0500

    Update package setup.

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
-"""Setup for z3c.namedfile package."""
+"""Installer for the z3c.namedfile package."""
 
-from setuptools import (
-    find_packages,
-    setup,
-)
+from setuptools import find_packages
+from setuptools import setup
+
 
 version = '0.4.dev0'
-description = 'File types and fields for images, files and blob files ' \
-              'with filenames.'
-
-long_description = ('\n'.join([
+description = (
+    'File types and fields for images, files and blob files with '
+    'filenames.'
+)
+long_description = '\n\n'.join([
     open('README.rst').read(),
-    'Contributors',
-    '------------\n',
     open('CONTRIBUTORS.rst').read(),
     open('CHANGES.rst').read(),
-]))
+])
 
 install_requires = [
     'setuptools',
     # -*- Extra requirements: -*-
     'zope.app.file',
+]
+
+test_requires = [
+    'Pillow',
 ]
 
 setup(
@@ -30,39 +32,37 @@ setup(
     description=description,
     long_description=long_description,
     # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Zope3',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Zope",
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Zope',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='zope named file image blob',
-    author='Thomas Massmann',
-    author_email='thomas.massmann@it-spir.it',
-    url='https://bitbucket.org/it_spirit/z3c.namedfile',
-    download_url='http://pypi.python.org/pypi/z3c.namedfile',
-    license='BSD',
+    author='it-spirit',
+    author_email='development@it-spir.it',
+    url='https://github.com/it-spirit/z3c.namedfile',
+    download_url='https://pypi.python.org/pypi/z3c.namedfile',
+    license='GPL version 2',
     packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'': 'src'},
     namespace_packages=['z3c'],
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    extras_require=dict(
-        blobs=[
+    extras_require={
+        'blobs': [
             'z3c.blobfile',
         ],
-        test=[
-            'Pillow',
-        ],
-    ),
+        'test': test_requires,
+    },
 )
